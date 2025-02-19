a6b874dc65649266c6210f06e144ce1f66d29878
Md Wahiduzzaman Bappy <wahidbappy253@gmail.com>
2025-02-20 04:43:16 +0600

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,170 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_vision/flutter_vision.dart';
+
+class YoloVideo extends StatefulWidget {
+  const YoloVideo({super.key});
+
+  @override
+  State<YoloVideo> createState() => _YoloVideoState();
+}
+
+class _YoloVideoState extends State<YoloVideo> {
+  late CameraController controller;
+  late FlutterVision vision;
+  late List<Map<String, dynamic>> yoloResults = [];
+  CameraImage? cameraImage;
+  bool isLoaded = false;
+  bool isDetecting = false;
+  double confidenceThreshold = 0.5;
+  late List<CameraDescription> cameras;
+
+  @override
+  void initState() {
+    super.initState();
+    initCameraAndModel();
+  }
+
+  Future<void> initCameraAndModel() async {
+    cameras = await availableCameras();
+    vision = FlutterVision();
+    controller = CameraController(cameras[0], ResolutionPreset.high);
+    await controller.initialize();
+    await loadYoloModel();
+    setState(() {
+      isLoaded = true;
+      isDetecting = false;
+    });
+  }
+
+  Future<void> loadYoloModel() async {
+    await vision.loadYoloModel(
+      labels: 'assets/CLASSES.txt',
+      modelPath: 'assets/curr_float32.tflite',
+      modelVersion: "yolov8",
+      numThreads: 1,
+      useGpu: true,
+    );
+  }
+
+  Future<void> startDetection() async {
+    setState(() => isDetecting = true);
+    if (controller.value.isStreamingImages) return;
+
+    await controller.startImageStream((image) async {
+      if (isDetecting) {
+        cameraImage = image;
+        await yoloOnFrame(image);
+      }
+    });
+  }
+
+  Future<void> stopDetection() async {
+    setState(() {
+      isDetecting = false;
+      yoloResults.clear();
+    });
+  }
+
+  Future<void> yoloOnFrame(CameraImage image) async {
+    final result = await vision.yoloOnFrame(
+      bytesList: image.planes.map((plane) => plane.bytes).toList(),
+      imageHeight: image.height,
+      imageWidth: image.width,
+      iouThreshold: 0.4,
+      confThreshold: confidenceThreshold,
+      classThreshold: 0.5,
+    );
+
+    if (result.isNotEmpty) {
+      setState(() => yoloResults = result);
+    }
+  }
+
+  List<Widget> displayBoxesAroundRecognizedObjects(Size screen) {
+    if (yoloResults.isEmpty) return [];
+    double factorX = screen.width / (cameraImage?.height ?? 1);
+    double factorY = screen.height / (cameraImage?.width ?? 1);
+
+    return yoloResults.map((result) {
+      double objectX = result["box"][0] * factorX;
+      double objectY = result["box"][1] * factorY;
+      double objectWidth = (result["box"][2] - result["box"][0]) * factorX;
+      double objectHeight = (result["box"][3] - result["box"][1]) * factorY;
+
+      return Positioned(
+        left: objectX,
+        top: objectY,
+        width: objectWidth,
+        height: objectHeight,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(color: Colors.pink, width: 2.0),
+          ),
+          child: Text(
+            "${result['tag']} ${(result['box'][4] * 100).toStringAsFixed(1)}%",
+            style: TextStyle(
+              background: Paint()..color = Colors.green,
+              color: Colors.white,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    if (!isLoaded) {
+      return const Scaffold(
+        body: Center(child: Text("Model not loaded, waiting for it")),
+      );
+    }
+
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: CameraPreview(controller),
+          ),
+          ...displayBoxesAroundRecognizedObjects(size),
+          Positioned(
+            bottom: 75,
+            width: size.width,
+            child: Center(
+              child: Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 5, color: Colors.white),
+                ),
+                child: IconButton(
+                  onPressed: isDetecting ? stopDetection : startDetection,
+                  icon: Icon(
+                    isDetecting ? Icons.stop : Icons.play_arrow,
+                    color: isDetecting ? Colors.red : Colors.white,
+                  ),
+                  iconSize: 50,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    vision.closeYoloModel();
+    super.dispose();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'app.dart';
+
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  runApp(
+    const MaterialApp(
+      home: YoloVideo(),
+    ),
+  );
+}
